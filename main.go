d2c18cebf5b0b09c8b0bf8bc21771a4c42e6eb5e
Elvis Raul Estevan <elvis.estevan65@gmail.com>
2025-03-29 19:53:34 -0300

    my first api in go

--- main.go
+++ main.go
@@ -1,7 +1,38 @@
 package main
 
-import "fmt"
+import (
+	"encoding/json"
+	"log"
+	"net/http"
+)
+
+type TodoItem struct {
+	Item string `json:"item"`
+}
 
 func main() {
-	fmt.Println("Hello World")
+	mux := http.NewServeMux()
+
+	todos := make([]TodoItem, 0)
+
+	mux.HandleFunc("GET /todo", func(w http.ResponseWriter, r *http.Request) {
+		w.Header().Set("Content-Type", "application/json")
+		if err := json.NewEncoder(w).Encode(todos); err != nil {
+			http.Error(w, err.Error(), http.StatusInternalServerError)
+		}
+	})
+
+	mux.HandleFunc("POST /todo", func(w http.ResponseWriter, r *http.Request) {
+		var todoItem TodoItem
+		if err := json.NewDecoder(r.Body).Decode(&todoItem); err != nil {
+			http.Error(w, err.Error(), http.StatusBadRequest)
+			return
+		}
+		todos = append(todos, todoItem)
+		w.WriteHeader(http.StatusCreated)
+	})
+
+	if err := http.ListenAndServe(":8080", mux); err != nil {
+		log.Fatal(err)
+	}
 }
